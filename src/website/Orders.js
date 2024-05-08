bbf5f7e9dedba266eb099f432c9c306c96003fdd
b9dn <mabogdan35@gmail.com>
2024-05-08 13:09:11 +0200

    font size

--- src/website/Orders.js
+++ src/website/Orders.js
@@ -1,5 +1,5 @@
 import { useLocation } from "react-router-dom";
 export const Orders = () => {
   const location = useLocation();
-  return <div style={{ fontSize: "10vw" }}>{location.state}</div>;
+  return <div style={{ fontSize: "70px" }}>{location.state}</div>;
 };
